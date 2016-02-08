35fc749899da9d644580b26000a6847fe87bf986
Nathan <nathanrichardng@gmail.com>
2016-02-07 19:27:44 -1000

    updated notification system

--- client/components/Notification/NotificationFrame.jsx
+++ client/components/Notification/NotificationFrame.jsx
@@ -1,64 +1,28 @@
 NotificationFrame = React.createClass({
-	
 	mixins: [ReactMeteorData],
-
 	getMeteorData() {
 		return {
-			success: Session.get("success-message"),
-			error: Session.get("error-message")
-		}
+	  		notifications: Collections.Notifications.find({}).fetch()
+	  	}
 	},
-
-	getInitialState() {
-		return {
-			timeout: false
-		}
+	renderNotifications() {
+		console.log("notifications", this.data.notifications);
+		return(
+			this.data.notifications.map(function(notification) {
+				return (
+					<Notification
+							key={notification._id}
+							message={notification.message}
+							className={notification.className + " notification"} />
+				)
+			})
+		)
 	},
-
-	renderNotification() {
-		if(this.data.error != "") {
-			this.updateTimeout();
-			return (
-				<Notification 
-					message={this.data.error}
-					className="entering error-message" />
-			)
-		}
-		else if(this.data.success != "") {
-			this.updateTimeout();
-			return (
-				<Notification
-					message={this.data.success}
-					className="entering success-message" />
-			)
-		}
-		else {
-			return (
-				<div></div>
-			)
-		}
-	},
-
-	updateTimeout() {
-		if(this.state.timeout) {
-			clearTimeout(this.state.timeout);
-		}
-		this.clearNotification();
-	},
-
-	clearNotification() {
-		//need to clean this up so its more fluid.
-		var that = this;
-		setTimeout(function() {
-			that.setState({ timeout: false });
-			Session.set("success-message", "");
-			Session.set("error-message", "");
-		}, 3000);
-	},
-
 	render() {
-		return (
-			this.renderNotification()
+		return(
+			<div className="notifications-box">
+				{this.renderNotifications()}
+			</div>
 		)
 	}
 });
